dd2901fbaa14517db6023adda3092fb7926f30b4
Fitria24 <72337273+Fitria24@users.noreply.github.com>
2020-10-22 17:36:32 +0700

    commit 3
    commit 3

--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/coba', function () {
-    return view('coba');
-});
-
-
-
 use App\Http\Controllers\CobaController;
-Route::get('/coba/{no}',[CobaController::class, 'coba']);
-Route::get('/test', [CobaController::class, 'index']);
-Route::get('/test/{ke}', [CobaController::class, 'urutan']);
+
+Route::get('',[CobaController::class, 'index']);
+Route::get('/friends', [CobaController::class, 'index']);
+
+Route::get('/friends/create', [CobaController::class, 'create']);
+
+Route::post('/friends', [CobaController::class, 'store']);
